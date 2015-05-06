815854cfdaebc9ad3f8b60fe19953565887fc870
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-05-05 20:26:35 -0700

    pass .all method spec

--- lib/task.rb
+++ lib/task.rb
@@ -6,6 +6,17 @@ class Task
 		@list_id = attributes.fetch(:list_id)
 	end
 
+	define_singleton_method(:all) do
+		returned_tasks =DB.exec("SELECT * FROM tasks;")
+		tasks = []
+		returned_tasks.each() do |task|
+			description =task.fetch("description")
+			list_id = task.fetch("list_id").to_i()
+			tasks.push(Task.new({:description => description, :list_id => list_id}))
+		end
+		tasks
+	end
+
 
 # define_method(:==) do |another_task|
 	# 	self.description().==(another_task.description())
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,13 +2,13 @@ require("rspec")
 require("pg")
 require("task")
 
-# DB = PG.connect({:dbname => "to_do_test"})
+DB = PG.connect({:dbname => "to_do_test"})
 
-# RSpec.configure do |config|
-# 	config.after(:each) do
-# 		DB.exec("DELETE FROM tasks *;")
-# 	end
-# end
+RSpec.configure do |config|
+	config.after(:each) do
+		DB.exec("DELETE FROM tasks *;")
+	end
+end
 
 describe(Task) do
 	# describe("#==") do
