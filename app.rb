afc7580dc5421cb093ef738742cd74daa3676658
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-05-06 18:49:46 -0700

    add app.rb file with index list_form list_success routes

---
+++ app.rb
@@ -0,0 +1,23 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/task')
+require('./lib/list')
+also_reload('lib/**/*.rb')
+require('pg')
+
+DB = PG.connect({:dbname => "to_do"})
+
+get("/") do
+	erb(:index)
+end
+
+get("/lists/new") do
+	erb(:list_form)
+end
+
+get("/lists") do
+	name = params.fetch("name")
+	list = List.new({:name => name, :id => nil})
+	list.save()
+	erb(:list_success)
+end
