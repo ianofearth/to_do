d087a850aebd6d4d54e9a0b20278b1a456ce2bcd
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-05-06 18:43:40 -0700

    add spec to test add new list function, add views folder

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,3 +2,14 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('adding a new list', {:type => :feature}) do
+	it('allows a user to click a list to see the tasks and details for it') do
+		visit('/')
+		click_link('Add New List')
+		fill_in('name', :with => 'epicodus work')
+		click_button('Add List')
+		expect(page).to have_content('Success!')
+	end
+	
+end
