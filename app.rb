5991007b5d68c24decd0cc3727c6d34398a4a26f
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-05-07 21:26:31 -0700

    add content to app.rb

--- app.rb
+++ app.rb
@@ -1,42 +1,48 @@
-# require('sinatra')
-# require('sinatra/reloader')
-# require('./lib/task')
-# require('./lib/list')
-# also_reload('lib/**/*.rb')
-# require('pg')
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/task')
+require('./lib/list')
+also_reload('lib/**/*.rb')
+require('pg')
 
-# #DB = PG.connect({:dbname => "to_do"}) #for use
+DB = PG.connect({:dbname => "to_do"}) #for use
 # DB = PG.connect({:dbname => "to_do_test"}) #for testing
 
-# get("/") do
-# 	erb(:index)
-# end
+get("/") do
+	erb(:index)
+end
 
-# get("/lists/new") do
-# 	erb(:list_form)
-# end
+get("/lists") do
+	@lists = List.all()
+	erb(:lists)
+end
 
-# post("/lists") do
-# 	description = params.fetch("description")
-# 	list = List.new({:description => description, :id => nil})
-# 	list.save()
-# 	erb(:list_success)
-# end
+get("/lists/new") do
+	erb(:list_form)
+end
 
-# get("/lists") do
-# 	@lists = List.all()
-# 	erb(:lists)
-# end
+post("/lists") do
+	name = params.fetch("name")
+	list = List.new({:name => name, :id => nil})
+	list.save()
+	erb(:list_success)
+end
 
-# get("/lists/:id") do
-# 	@list = List.find(params.fetch("id").to_i())
-# 	erb(:list)
-# end
+get("/lists/:id") do
+	@list = List.find(params.fetch("id").to_i())
+	erb(:list)
+end
 
-# post("/tasks") do
-# 	description = params.fetch("description")
-# 	list_id = params.fetch("list_id").to_i()
-# 	task = Task.new({:description => description, :list_id => list_id})
-# 	task.save()
-# 	erb(:success)
-# end
+get("/tasks/:id") do
+	@list = params.fetch("id").to_i()
+	erb(:add_task)
+end
+
+post("/tasks/:id") do
+	description = params.fetch("description")
+	list_id = params.fetch("id").to_i()
+	@list = List.find(list_id)
+	@task = Task.new({:description => description, :list_id => list_id})
+	@task.save()
+	erb(:task_success)
+end
